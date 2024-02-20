e632992d50d64389d9aa68d2bf0fff2d995c1048
Roar Harjo <roarharjo@gmail.com>
2024-02-20 12:14:10 +0100

    Edit in models.py

--- app/models.py
+++ app/models.py
@@ -1,10 +1,10 @@
 from app import db
 
 class Attraction(db.Model):
-    id = db.Column(db.int, primary_key=True)
-    name = db.Column(db.varchar(55), nullable=False)
-    location = db.Column(db.varchar(255), nullable=False)
-    description = db.Column(db.text, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(55), nullable=False)
+    location = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text, nullable=False)
 
     def __repr__(self):
         return f"Attraction('{self.name}', '{self.location}', '{self.description}')'"
