e6a53891aa77ce1f66f3a1d1c36d97cfaa415b49
Eirik <eirik@tenno.no>
2024-02-21 22:46:01 +0100

    Laget til Login og register (backend)

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,10 +1,44 @@
 from . import auth
-from flask import render_template, request, redirect, url_for, session
+from .forms import LoginForm, RegistrationForm
+from .. import db
+from ..models import User
+from flask import render_template, request, redirect, url_for, session, flash
+from flask_login import login_user, logout_user, login_required, current_user
+
 
 @auth.route('/register', methods=['GET', 'POST'])
-def user_register() -> 'html':
-	pass
+def register() -> 'html':
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		user = User(
+			username = form.username.data,
+			first_name = form.first_name.data,
+			last_name = form.last_name.data,
+			email = form.email.data
+		)
+		user.set_password(form.password.data)
+		db.session.add(user)
+		db.session.commit()
+		flash('User has been registrated.')
+		return redirect(url_for('auth.login'))
+	return render_template('auth/register.html', form=form)
+
 
 @auth.route('/login', methods=['GET', 'POST'])
-def user_login() -> 'html':
-	pass
+def login() -> 'html':
+	form = LoginForm()
+	if form.validate_on_submit():
+		user = User.query.filter_by(email=form.email.data).first()
+		if user is not None and user.verify_password(form.password.data):
+			login_user(user, form.remember_me.data)
+			return redirect(url_for('index'))
+		flash('Invalid username or password.')
+	return render_template('auth.login', form=form)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+	logout_user()
+	flash('You have been logged out.')
+	return redirect(url_for('index'))
