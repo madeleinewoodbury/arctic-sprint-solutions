86647cff8eb0e9f4bc09cc5c6726c1c036701786
bareola <ogiaev@gmail.com>
2024-02-21 21:17:48 +0100

    removed models.py from attractions bp

--- app/attractions/views.py
+++ app/attractions/views.py
@@ -1,6 +1,6 @@
-from flask import jsonify, render_template
+from flask import render_template
 from . import attractions
-from .models import Attraction
+from app.models import Attraction
 from app import db
 
 
