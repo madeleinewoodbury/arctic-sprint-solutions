636059cffc7f2239c320198118e63b72a024672e
Gudmundur <gru007@post.uit.no>
2024-02-23 09:36:52 +0100

    fixing some errors in login/registration forms

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,11 +1,12 @@
 from flask_wtf import FlaskForm as Form
-from wtforms import StringField, EmailField, PasswordField, SubmitField
+from wtforms import StringField, EmailField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, Length, EqualTo, ValidationError
 from ..models import User
 
 class LoginForm(Form):
 	email = EmailField('Email', validators=[DataRequired()])
 	password = PasswordField('Password', validators=[DataRequired()])
+	remember_me = BooleanField('Remember me?')
 	submit = SubmitField('Login')
 
 class RegistrationForm(Form):
@@ -14,7 +15,7 @@ class RegistrationForm(Form):
 	username = StringField('Username', validators=[DataRequired(), Length(min=6, max=50)])
 	email = EmailField('Email', validators=[DataRequired(), Length(max=50)])
 	password = PasswordField('Password', validators=[DataRequired(), Length(min=6, max=50)])
-	confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+	confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), Length(min=6, max=50), EqualTo('password')])
 	submit = SubmitField('Register')
  
 	def validate_username(self, username):
@@ -26,20 +27,3 @@ class RegistrationForm(Form):
 		user = User.query.filter_by(email=email.data).first()
 		if user is not None:
 			raise ValidationError('This email is already taken.')
-
-
-'''
-class User(UserMixin, db.Model):
-	__tablename__ = 'user'
-	id = db.Column(db.Integer, primary_key=True)
-	username = db.Column(db.String(50))
-	first_name = db.Column(db.String(50))
-	last_name = db.Column(db.String(50))
-	email = db.Column(db.String(50), unique=True)
-	password = db.Column(db.String(255), nullable=False)
-	role = db.Column(db.Integer, db.ForeignKey('userRole.id'))
-	created_at = db.Column(db.DateTime, default=datetime.utcnow)
-
-	user = User.query.filter_by(email=form.email.data).first()
- 
-'''
