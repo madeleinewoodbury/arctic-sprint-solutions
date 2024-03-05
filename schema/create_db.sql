9c48ba520092ef9ee9854634c7e4a075258d39af
Madeleine Woodbury <mail@woodburycode.com>
2024-03-05 13:20:55 -0500

    add admin user

--- schema/create_db.sql
+++ schema/create_db.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: db
--- Generation Time: Mar 05, 2024 at 11:34 AM
--- Server version: 11.2.3-MariaDB-1:11.2.3+maria~ubu2204
--- PHP Version: 8.1.17
+-- Generation Time: Mar 05, 2024 at 06:16 PM
+-- Server version: 10.11.2-MariaDB-1:10.11.2+maria~ubu2204
+-- PHP Version: 8.1.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -54,7 +54,6 @@ DROP TABLE IF EXISTS userRole;
 DROP TABLE IF EXISTS city;
 DROP TABLE IF EXISTS country;
 DROP TABLE IF EXISTS language;
-
 --
 -- Table structure for table `achievement`
 --
@@ -163,10 +162,10 @@ CREATE TABLE `attractionCategory` (
 --
 
 INSERT INTO `attractionCategory` (`category_id`, `attraction_id`) VALUES
-(9, 1),
 (3, 2),
 (7, 3),
-(8, 3);
+(8, 3),
+(9, 1);
 
 -- --------------------------------------------------------
 
@@ -198,14 +197,14 @@ CREATE TABLE `attractionTag` (
 --
 
 INSERT INTO `attractionTag` (`tag_id`, `attraction_id`) VALUES
-(5, 1),
-(17, 1),
 (1, 2),
 (3, 2),
-(8, 2),
-(17, 2),
 (3, 3),
+(5, 1),
 (7, 3),
+(8, 2),
+(17, 1),
+(17, 2),
 (17, 3);
 
 -- --------------------------------------------------------
@@ -245,35 +244,15 @@ CREATE TABLE `city` (
   `id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
   `description` text DEFAULT NULL,
-  `image` varchar(255) DEFAULT NULL,
-  `country` int(11) NOT NULL
+  `image` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 
 --
 -- Dumping data for table `city`
 --
 
-INSERT INTO `city` (`id`, `name`, `description`, `image`, `country`) VALUES
-(1, 'Tromsø', 'Tromsø, a vibrant city surrounded by majestic mountains and the Arctic landscape, is known as the \'Gateway to the Arctic Sea.\' The city offers a unique blend of culture, history, and nature, with attractions such as the Arctic Cathedral, Polaria, and opportunities for Northern Lights safaris and outdoor activities', 'https://images.unsplash.com/photo-1609538205185-40f2701cf7d6?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `country`
---
-
-CREATE TABLE `country` (
-  `id` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `code` char(2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `country`
---
-
-INSERT INTO `country` (`id`, `name`, `code`) VALUES
-(1, 'Norway', 'NO');
+INSERT INTO `city` (`id`, `name`, `description`, `image`) VALUES
+(1, 'Tromsø', 'Tromsø, a vibrant city surrounded by majestic mountains and the Arctic landscape, is known as the \'Gateway to the Arctic Sea.\' The city offers a unique blend of culture, history, and nature, with attractions such as the Arctic Cathedral, Polaria, and opportunities for Northern Lights safaris and outdoor activities', 'https://images.unsplash.com/photo-1609538205185-40f2701cf7d6?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
 
 -- --------------------------------------------------------
 
@@ -363,9 +342,16 @@ CREATE TABLE `user` (
   `last_name` varchar(50) DEFAULT NULL,
   `email` varchar(50) DEFAULT NULL,
   `password` varchar(255) NOT NULL,
-  `role` int(11) DEFAULT NULL,
+  `role` int(11) DEFAULT 2,
   `created_at` timestamp NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `first_name`, `last_name`, `email`, `password`, `role`, `created_at`) VALUES
+(4, 'superuser', 'Ford', 'Prefect', 'admin@mail.com', 'scrypt:32768:8:1$bfcAMyr9DEPRSxwK$20d93afdb473b205d458a6106d86a935600a61f9685f8912df601f8b66f11fc376a13d860d876ef0c8a92cb15ddd33daf01cc264da390fe93c03b5969464a96a', 1, '2024-03-05 18:13:57');
 
 -- --------------------------------------------------------
 
@@ -378,41 +364,6 @@ CREATE TABLE `userAchievement` (
   `user_id` int(11) NOT NULL,
   `time_achieved` timestamp NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `userAgeGroupPreference`
---
-
-CREATE TABLE `userAgeGroupPreference` (
-  `user_id` int(11) NOT NULL,
-  `age_group_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `userCategoryPreference`
---
-
-CREATE TABLE `userCategoryPreference` (
-  `user_id` int(11) NOT NULL,
-  `category_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `userProfile`
---
-
-CREATE TABLE `userProfile` (
-  `id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `bio` text DEFAULT NULL,
-  `country` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -432,18 +383,8 @@ CREATE TABLE `userRole` (
 
 INSERT INTO `userRole` (`id`, `title`, `is_admin`) VALUES
 (1, 'Administrator', 1),
-(2, 'Bruker', 0);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `userTagPreference`
---
-
-CREATE TABLE `userTagPreference` (
-  `user_id` int(11) NOT NULL,
-  `tag_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+(2, 'Bruker', 0),
+(3, 'Moderator', 1);
 
 -- --------------------------------------------------------
 
@@ -518,13 +459,6 @@ ALTER TABLE `category`
 -- Indexes for table `city`
 --
 ALTER TABLE `city`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_city_country` (`country`);
-
---
--- Indexes for table `country`
---
-ALTER TABLE `country`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -569,39 +503,10 @@ ALTER TABLE `userAchievement`
   ADD KEY `user_id` (`user_id`);
 
 --
--- Indexes for table `userAgeGroupPreference`
---
-ALTER TABLE `userAgeGroupPreference`
-  ADD PRIMARY KEY (`user_id`,`age_group_id`),
-  ADD KEY `age_group_id` (`age_group_id`);
-
---
--- Indexes for table `userCategoryPreference`
---
-ALTER TABLE `userCategoryPreference`
-  ADD PRIMARY KEY (`user_id`,`category_id`),
-  ADD KEY `category_id` (`category_id`);
-
---
--- Indexes for table `userProfile`
---
-ALTER TABLE `userProfile`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `user_id` (`user_id`),
-  ADD KEY `country` (`country`);
-
---
 -- Indexes for table `userRole`
 --
 ALTER TABLE `userRole`
   ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `userTagPreference`
---
-ALTER TABLE `userTagPreference`
-  ADD PRIMARY KEY (`user_id`,`tag_id`),
-  ADD KEY `tag_id` (`tag_id`);
 
 --
 -- Indexes for table `visitedAttraction`
@@ -651,12 +556,6 @@ ALTER TABLE `city`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `country`
---
-ALTER TABLE `country`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
 -- AUTO_INCREMENT for table `language`
 --
 ALTER TABLE `language`
@@ -672,19 +571,13 @@ ALTER TABLE `tag`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `userProfile`
---
-ALTER TABLE `userProfile`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `userRole`
 --
 ALTER TABLE `userRole`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
@@ -724,12 +617,6 @@ ALTER TABLE `attractionTag`
   ADD CONSTRAINT `attractionTag_ibfk_2` FOREIGN KEY (`attraction_id`) REFERENCES `attraction` (`id`) ON DELETE CASCADE;
 
 --
--- Constraints for table `city`
---
-ALTER TABLE `city`
-  ADD CONSTRAINT `fk_city_country` FOREIGN KEY (`country`) REFERENCES `country` (`id`);
-
---
 -- Constraints for table `friendship`
 --
 ALTER TABLE `friendship`
@@ -755,34 +642,6 @@ ALTER TABLE `user`
 ALTER TABLE `userAchievement`
   ADD CONSTRAINT `userAchievement_ibfk_1` FOREIGN KEY (`achievement_id`) REFERENCES `achievement` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `userAchievement_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE;
-
---
--- Constraints for table `userAgeGroupPreference`
---
-ALTER TABLE `userAgeGroupPreference`
-  ADD CONSTRAINT `userAgeGroupPreference_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
-  ADD CONSTRAINT `userAgeGroupPreference_ibfk_2` FOREIGN KEY (`age_group_id`) REFERENCES `ageGroup` (`id`);
-
---
--- Constraints for table `userCategoryPreference`
---
-ALTER TABLE `userCategoryPreference`
-  ADD CONSTRAINT `userCategoryPreference_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
-  ADD CONSTRAINT `userCategoryPreference_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`);
-
---
--- Constraints for table `userProfile`
---
-ALTER TABLE `userProfile`
-  ADD CONSTRAINT `userProfile_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
-  ADD CONSTRAINT `userProfile_ibfk_2` FOREIGN KEY (`country`) REFERENCES `country` (`id`);
-
---
--- Constraints for table `userTagPreference`
---
-ALTER TABLE `userTagPreference`
-  ADD CONSTRAINT `userTagPreference_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
-  ADD CONSTRAINT `userTagPreference_ibfk_2` FOREIGN KEY (`tag_id`) REFERENCES `tag` (`id`);
 
 --
 -- Constraints for table `visitedAttraction`
