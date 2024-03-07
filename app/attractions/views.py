90e2522d682b0f991d1c2217d5d706489812d5fb
Madeleine Woodbury <mail@woodburycode.com>
2024-03-06 20:03:19 -0500

    Send all attraction infor to attractions template

--- app/attractions/views.py
+++ app/attractions/views.py
@@ -4,21 +4,11 @@ from app.models import Attraction
 from app import db
 
 
+# Get all attractions
 @attractions.route('/attractions', methods=['GET'])
 def get_attractions():
     attractions = db.session.query(Attraction).all()
-    attraction_list = []
-    for attraction in attractions:
-        attraction_data = {
-            'id': attraction.id,
-            'name': attraction.name,
-            'description': attraction.description,
-            'location': attraction.location,
-            'image': attraction.image,
-            'points': attraction.points
-        }
-        attraction_list.append(attraction_data)
-    return render_template('attractions.html', attractions=attraction_list)
+    return render_template('attractions.html', attractions=attractions)
 
 
 # Get single attraction
