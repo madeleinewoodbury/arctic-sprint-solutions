340d14949ea10100f012702cd6536ce2f2484448
Gudmundur <gru007@post.uit.no>
2024-03-16 14:54:31 +0100

    Remove unusued elements from attraction forms

--- app/attractions/forms.py
+++ app/attractions/forms.py
@@ -1,13 +1,13 @@
 from flask_wtf import FlaskForm as Form
 from wtforms import StringField, SelectField, SubmitField, SelectMultipleField, SubmitField, widgets
-from app.models import Category
 
 class SearchForm(Form):
-  search_text = StringField('Search Attractions', render_kw={'placeholder': 'Search Attractions'})
-  category = SelectField('Category', choices=[('cat1', 'Category 1'), ('cat2', 'Category 2')], render_kw={'placeholder': 'Category'})
-  age_group = SelectField('Age Group', choices=[('group1', 'Group 1'), ('group2', 'Group 2'), ('group3', 'Group 3')], render_kw={'placeholder': 'Age Groups'})
-  tag = SelectField('Tag', choices=[('tag1', 'Tag 1'), ('tag2', 'Tag 2'), ('tag3', 'Tag 3')], render_kw={'placeholder': 'Keyword'})
-  submit = SubmitField('Search', render_kw={'class': 'search-btn'})
+    search_text = StringField('Search Attractions', render_kw={'placeholder': 'Search Attractions'})
+    # Remove commented SelectFields if we decide to not use them.
+    #category = SelectField('Category', render_kw={'placeholder': 'Category'})
+    #age_group = SelectField('Age Group', choices=[('group1', 'Group 1'), ('group2', 'Group 2'), ('group3', 'Group 3')], render_kw={'placeholder': 'Age Groups'})
+    #tag = SelectField('Tag', choices=[('tag1', 'Tag 1'), ('tag2', 'Tag 2'), ('tag3', 'Tag 3')], render_kw={'placeholder': 'Keyword'})
+    submit = SubmitField('Search', render_kw={'class': 'search-btn'})
 
 class MultiCheckboxField(SelectMultipleField):
     widget = widgets.ListWidget(html_tag='ul', prefix_label=False)
@@ -15,7 +15,6 @@ class MultiCheckboxField(SelectMultipleField):
     
 class FilterAttractionsForm(Form):
     categories = MultiCheckboxField('Category', coerce=int)
+    age_groups = MultiCheckboxField('Age group', coerce=int)
+    tags = MultiCheckboxField('Tag', coerce=int)
     
-    def __init__(self, *args, **kwargs):
-        super(FilterAttractionsForm, self).__init__(*args, **kwargs)
-        self.categories.choices = [(category.id, category.name) for category in Category.query.all()]
