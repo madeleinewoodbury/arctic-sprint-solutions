3abdaf7c7286a3bce812c1f2ce334aaba048a36a
bareola <ogiaev@gmail.com>
2024-04-15 13:22:07 +0200

    Lagt til mulighet å endre/sette preferanser i agegroup og category

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -4,66 +4,83 @@ from wtforms.validators import DataRequired, Length, EqualTo, ValidationError, R
 from app.models import User
 from flask_babel import lazy_gettext as _
 
+
 class LoginForm(Form):
-	email = StringField(_('Email'), validators=[DataRequired(), Email()], render_kw={"autocomplete": "username"})
-	password = PasswordField(_('Password'), validators=[DataRequired()])
-	remember_me = BooleanField(_('Remember me'))
-	submit = SubmitField(_('Login'), render_kw={'class': 'btn btn-primary'})
+    email = StringField(_('Email'), validators=[
+                        DataRequired(), Email()], render_kw={"autocomplete": "username"})
+    password = PasswordField(_('Password'), validators=[DataRequired()])
+    remember_me = BooleanField(_('Remember me'))
+    submit = SubmitField(_('Login'), render_kw={'class': 'btn btn-primary'})
+
 
 class RegistrationForm(Form):
-	country= SelectField(_('Country'))
-	first_name = StringField(_('First Name'), validators=[DataRequired(), Length(max=50)])
-	last_name = StringField(_('Last Name'), validators=[DataRequired(), Length(max=50)])
-	username = StringField(_('Username'), validators=[
-    	DataRequired(), Length(min=6, max=20), 
-     	Regexp('^[a-zA-Z0-9_]+$', 
-            message=_('Username must be at least 10 characters long and only contain letters, numbers, and underscores.'))])
-	email = StringField(_('Email'), validators=[DataRequired(), Email(), Length(max=50)], render_kw={"autocomplete": "username"})
-	password = PasswordField('Password', validators=[
-    	DataRequired(), Length(min=10, max=40), 
-      	Regexp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{10,40}$', 
-            message=_('Password must be at least 10 characters long while containing at least one uppercase letter, one lowercase letter, and one number.'))])
-	confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), Length(min=10, max=40), EqualTo('password')])
-	submit = SubmitField(_('Register'), render_kw={'class': 'btn btn-primary'})
- 
-	def validate_username(self, username):
-		user = User.query.filter_by(username=username.data).first()
-		if user is not None:
-			raise ValidationError(_('This username is already taken.'))
+    country = SelectField(_('Country'))
+    first_name = StringField(_('First Name'), validators=[
+                             DataRequired(), Length(max=50)])
+    last_name = StringField(_('Last Name'), validators=[
+                            DataRequired(), Length(max=50)])
+    username = StringField(_('Username'), validators=[
+        DataRequired(), Length(min=6, max=20),
+        Regexp('^[a-zA-Z0-9_]+$',
+               message=_('Username must be at least 10 characters long and only contain letters, numbers, and underscores.'))])
+    email = StringField(_('Email'), validators=[DataRequired(), Email(), Length(
+        max=50)], render_kw={"autocomplete": "username"})
+    password = PasswordField('Password', validators=[
+        DataRequired(), Length(min=10, max=40),
+        Regexp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{10,40}$',
+               message=_('Password must be at least 10 characters long while containing at least one uppercase letter, one lowercase letter, and one number.'))])
+    confirm_password = PasswordField('Confirm Password', validators=[
+                                     DataRequired(), Length(min=10, max=40), EqualTo('password')])
+    submit = SubmitField(_('Register'), render_kw={'class': 'btn btn-primary'})
 
-	def validate_email(self, email):
-		user = User.query.filter_by(email=email.data).first()
-		if user is not None:
-			raise ValidationError(_('This email is already taken.'))
-		
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError(_('This username is already taken.'))
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError(_('This email is already taken.'))
+
+
 class PasswordResetRequestForm(Form):
-	email = StringField(_('Email'), validators=[DataRequired(), Length(1,64), Email()])
-	submit = SubmitField(_('Reset Password'), render_kw={'class': 'btn btn-primary'})
+    email = StringField(_('Email'), validators=[
+                        DataRequired(), Length(1, 64), Email()])
+    submit = SubmitField(_('Reset Password'), render_kw={
+                         'class': 'btn btn-primary'})
 
 
 class PasswordResetForm(Form):
-	password = PasswordField('Password', validators=[
-    	DataRequired(), Length(min=10, max=40), 
-      	Regexp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{10,40}$', 
-            message=_('Password must be at least 10 characters long while containing at least one uppercase letter, one lowercase letter, and one number.'))])
-	confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), Length(min=10, max=40), EqualTo('password')])
-	submit = SubmitField(_('Reset Password'), render_kw={'class': 'btn btn-primary'})
+    password = PasswordField('Password', validators=[
+        DataRequired(), Length(min=10, max=40),
+        Regexp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{10,40}$',
+               message=_('Password must be at least 10 characters long while containing at least one uppercase letter, one lowercase letter, and one number.'))])
+    confirm_password = PasswordField('Confirm Password', validators=[
+                                     DataRequired(), Length(min=10, max=40), EqualTo('password')])
+    submit = SubmitField(_('Reset Password'), render_kw={
+                         'class': 'btn btn-primary'})
 
 
 class ProfileForm(Form):
     email = StringField(_('Email'), validators=[DataRequired(), Email()])
     gravatar_url = HiddenField(_('Gravatar URL'))
+    category = SelectMultipleField(_('Categories'), coerce=int)
     tag = SelectMultipleField(_('Select Tags'), coerce=int)
+    age_group = SelectMultipleField(_('Age Groups'), coerce=int)
     country = SelectField(_('Country'), choices=[])
-    submit = SubmitField(_('Update profile'), render_kw={'class': 'btn btn-primary'})
+    submit = SubmitField(_('Update profile'), render_kw={
+                         'class': 'btn btn-primary'})
 
 
 class SearchUsersForm(Form):
-    search_text = StringField(_('Search users'), render_kw={'placeholder': _('Search users')})
-    submit = SubmitField(_('Search'), render_kw={'class': 'btn btn-primary search-btn'})
-    
-    
+    search_text = StringField(_('Search users'), render_kw={
+                              'placeholder': _('Search users')})
+    submit = SubmitField(_('Search'), render_kw={
+                         'class': 'btn btn-primary search-btn'})
+
+
 class FriendRequestForm(Form):
-    search_text = StringField(_('Search users'), render_kw={'placeholder': _('Search users')})
+    search_text = StringField(_('Search users'), render_kw={
+                              'placeholder': _('Search users')})
     submit = SubmitField(_('Search'), render_kw={'class': 'search-btn'})
-    
--- app/auth/views.py
+++ app/auth/views.py
@@ -9,10 +9,12 @@ from flask_babel import _
 from datetime import datetime
 from ..email import send_email
 
+
 @auth.route('/register', methods=['GET', 'POST'])
 def register() -> 'html':
     form = RegistrationForm()
-    form.country.choices = [(country.id, country.name) for country in Country.query.all()]
+    form.country.choices = [(country.id, country.name)
+                            for country in Country.query.all()]
 
     if form.validate_on_submit():
         user = User(
@@ -33,22 +35,22 @@ def register() -> 'html':
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login() -> 'html':
-	form = LoginForm()
-	if form.validate_on_submit():
-		user = User.query.filter_by(email=form.email.data).first()
-		if user is not None and user.check_password(form.password.data):
-			login_user(user, form.remember_me.data)
-			return redirect(url_for('attractions.get_attractions'))
-		flash(_('Invalid username or password.'))
-	return render_template('login.html', form=form)
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.check_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            return redirect(url_for('attractions.get_attractions'))
+        flash(_('Invalid username or password.'))
+    return render_template('login.html', form=form)
 
 
 @auth.route('/logout')
 @login_required
 def logout():
-	logout_user()
-	flash(_('You have been logged out.'))
-	return redirect(url_for('main.index'))
+    logout_user()
+    flash(_('You have been logged out.'))
+    return redirect(url_for('main.index'))
 
 
 @auth.route('/reset', methods=['GET', 'POST'])
@@ -61,9 +63,10 @@ def password_reset_request():
         if user:
             token = user.generate_reset_token()
             send_email(user.email, 'Reset your password', 'email/reset_password',
-                        user=user, token=token)
-            flash(_("An email with instructions to reset your password has been sent to you."))
-            return(redirect(url_for('auth.login')))
+                       user=user, token=token)
+            flash(
+                _("An email with instructions to reset your password has been sent to you."))
+            return (redirect(url_for('auth.login')))
     return render_template('reset_password_request.html', form=form)
 
 
@@ -78,91 +81,132 @@ def password_reset(token):
             flash('Your password has been updated.')
             return redirect(url_for('auth.login'))
         else:
-            return(redirect(url_for('auth.login')))  
+            return (redirect(url_for('auth.login')))
     return render_template('reset_password.html', form=form)
 
+
 def update_user_profile(profile_form, activeTab):
-    # Get the user based on the provided email
     user = User.query.filter_by(email=profile_form.email.data).first()
 
-    # Update user preferences if user is found
     if user:
         selected_tag_ids = profile_form.tag.data
+        selected_category_ids = profile_form.category.data
+        selected_age_group_ids = profile_form.age_group.data
 
-        # Delete existing user preferences
-        UserTagPreference.query.filter_by(user_id=user.id).delete()
+        # Ensure transactional integrity
+        try:
+            UserTagPreference.query.filter_by(user_id=user.id).delete()
+            UserCategoryPreference.query.filter_by(user_id=user.id).delete()
+            UserAgeGroupPreference.query.filter_by(user_id=user.id).delete()
 
-        # Create new user preferences
-        for tag_id in selected_tag_ids:
-            user_preference = UserTagPreference(user_id=user.id, tag_id=tag_id)
-            db.session.add(user_preference)
-        
-        # Update user profile
-        user.country_id = profile_form.country.data
+            db.session.flush()
 
-        db.session.commit()
-        flash(_('Your preferences have been updated!'), 'success')
-        return redirect(url_for('auth.profile', current_tab=activeTab))  # Redirect to avoid form resubmission
+            for tag_id in selected_tag_ids:
+                user_preference = UserTagPreference(
+                    user_id=user.id, tag_id=tag_id)
+                db.session.add(user_preference)
+
+            for category_id in selected_category_ids:
+                user_preference = UserCategoryPreference(
+                    user_id=user.id, category_id=category_id)
+                db.session.add(user_preference)
+
+            for age_group_id in selected_age_group_ids:
+                user_preference = UserAgeGroupPreference(
+                    user_id=user.id, age_group_id=age_group_id)
+                db.session.add(user_preference)
+
+            user.country_id = profile_form.country.data
+
+            db.session.commit()
+            flash(_('Your preferences have been updated!'), 'success')
+            return redirect(url_for('auth.profile', current_tab=activeTab))
+        except Exception as e:
+            db.session.rollback()
+            flash(_('Error updating user preferences: ') + str(e), 'error')
     else:
         flash(_('User with provided email does not exist'), 'error')
 
 
 def get_user_preferences():
-    user_tag_preferences = UserTagPreference.query.filter_by(user_id=current_user.id).all()
-    user_preferences = [Tag.query.get(preference.tag_id).name for preference in user_tag_preferences]
-    
+    # Fetch user's tag preferences
+    user_tag_preferences = UserTagPreference.query.filter_by(
+        user_id=current_user.id).all()
+    tag_preferences = [Tag.query.get(
+        preference.tag_id).name for preference in user_tag_preferences]
+
+    # Fetch user's category preferences
+    user_category_preferences = UserCategoryPreference.query.filter_by(
+        user_id=current_user.id).all()
+    category_preferences = [Category.query.get(
+        preference.category_id).name for preference in user_category_preferences]
+
+    # Fetch user's age group preferences
+    user_age_group_preferences = UserAgeGroupPreference.query.filter_by(
+        user_id=current_user.id).all()
+    age_group_preferences = [AgeGroup.query.get(
+        preference.age_group_id).name for preference in user_age_group_preferences]
+
+    user_preferences = {
+        'tags': tag_preferences,
+        'categories': category_preferences,
+        'age_groups': age_group_preferences
+    }
+
     return user_preferences
 
 
 def search_users(search_text):
-     users = User.query.filter(User.username.contains(search_text)) \
-            .filter(User.id != current_user.id).all()
+    users = User.query.filter(User.username.contains(search_text)) \
+        .filter(User.id != current_user.id).all()
 
-     return users
+    return users
 
 
 def get_users_requesting():
     users_requesting = User.query.join(User.initiated_friendships) \
-                             .filter(Friendship.user_2 == current_user.id,
-                                     Friendship.status == 'pending') \
-                             .all()
+        .filter(Friendship.user_2 == current_user.id,
+                Friendship.status == 'pending') \
+        .all()
 
     return users_requesting
 
 
 def get_users_awaiting():
     users_awaiting = User.query.join(User.received_friendships) \
-                             .filter(Friendship.user_1 == current_user.id,
-                                     Friendship.status == 'pending') \
-                             .all()
+        .filter(Friendship.user_1 == current_user.id,
+                Friendship.status == 'pending') \
+        .all()
 
     return users_awaiting
 
 
-def get_firends():    
+def get_firends():
     users = User.query.join(User.initiated_friendships) \
-                   .filter(
-                       (Friendship.user_1 == current_user.id) | (Friendship.user_2 == current_user.id),
-                       Friendship.status == 'accepted') \
-                   .union(
-                       User.query.join(User.received_friendships) \
-                           .filter(
-                               (Friendship.user_1 == current_user.id) | (Friendship.user_2 == current_user.id),
-                               Friendship.status == 'accepted') \
-                           ).filter(User.id != current_user.id).all()
-    
+        .filter(
+        (Friendship.user_1 == current_user.id) | (
+            Friendship.user_2 == current_user.id),
+        Friendship.status == 'accepted') \
+        .union(
+        User.query.join(User.received_friendships)
+        .filter(
+            (Friendship.user_1 == current_user.id) | (
+                Friendship.user_2 == current_user.id),
+            Friendship.status == 'accepted')
+    ).filter(User.id != current_user.id).all()
+
     friends = []
     # Get attraction info from friend
     for user in users:
         friend = {
-               'user': user,
-               'visited': get_visited_attractions(user.id),
+            'user': user,
+            'visited': get_visited_attractions(user.id),
             #    'country': Country.query.get(user.country_id)
-         }  
-        friend['points'] = sum(item['attraction'].points for item in friend['visited'])
+        }
+        friend['points'] = sum(
+            item['attraction'].points for item in friend['visited'])
         friend['level'] = get_user_level(friend['points']).get('current_level')
         friends.append(friend)
-
 
     friends = sorted(friends, key=lambda x: x['points'], reverse=True)
     return friends
@@ -179,6 +223,7 @@ def get_visited_attractions(user_id):
 
     return visited_attractions
 
+
 def get_user_badge_progress(user_id):
     badges = Badge.query.all()
     user_progression = []
@@ -186,28 +231,29 @@ def get_user_badge_progress(user_id):
     for badge in badges:
         # Hent nødvendige tags for denne badgen
         total_visited_count = 0
-        requirements = BadgeRequirement.query.filter_by(badge_id=badge.id).all()
+        requirements = BadgeRequirement.query.filter_by(
+            badge_id=badge.id).all()
         badge_progress = {
-                'badge_name': badge.name, 
-                'tags_progress': [],
-                'description': badge.description,
-                'total_visited_count': 0
-                }
+            'badge_name': badge.name,
+            'tags_progress': [],
+            'description': badge.description,
+            'total_visited_count': 0
+        }
 
         for req in requirements:
             # Finn navnet på taggen
             tag = Tag.query.get(req.tag_id)
             tag_name = tag.name if tag else 'Unknown Tag'
-            
+
             # Telle antall besøkte attraksjoner som matcher denne taggen
             visited_count = db.session.query(db.func.count(VisitedAttraction.attraction_id)
-            ).join(AttractionTag, VisitedAttraction.attraction_id == AttractionTag.attraction_id
-            ).filter(VisitedAttraction.user_id == user_id, AttractionTag.tag_id == req.tag_id
-            ).scalar() or 0
-            
+                                             ).join(AttractionTag, VisitedAttraction.attraction_id == AttractionTag.attraction_id
+                                                    ).filter(VisitedAttraction.user_id == user_id, AttractionTag.tag_id == req.tag_id
+                                                             ).scalar() or 0
+
             # Oppdaterer total_visited_count
             total_visited_count += visited_count
-            
+
             badge_progress['tags_progress'].append({
                 'tag_name': tag_name,
                 'visited_count': visited_count,
@@ -218,7 +264,8 @@ def get_user_badge_progress(user_id):
         user_progression.append(badge_progress)
 
     # Sorterer basert på total_visited_count, fra høyeste til laveste slik at badgen man har høyest progresjon på vises først
-    user_progress = sorted(user_progression, key=lambda x: x['total_visited_count'], reverse=True)
+    user_progress = sorted(
+        user_progression, key=lambda x: x['total_visited_count'], reverse=True)
 
     return user_progress
 
@@ -226,27 +273,27 @@ def get_user_badge_progress(user_id):
 def get_user_level(points):
     BASE_POINTS = 42
     GROWTH_RATE = 1.05
-    
+
     current_level = 1
     points_required = BASE_POINTS
     points_remaining = points
-    
+
     while points_remaining >= points_required:
         points_remaining -= points_required
         current_level += 1
-        points_required = int((points_required + BASE_POINTS) * GROWTH_RATE - points_required)
-        
+        points_required = int((points_required + BASE_POINTS)
+                              * GROWTH_RATE - points_required)
+
     points_missing = points_required - points_remaining
-    
+
     level = {
         "current_level": current_level,
         "points_required": points_required,
         "points_missing": points_missing,
         "progress": points_remaining
     }
-    
+
     return level
-    
 
 
 @auth.route('/profile', methods=['GET', 'POST'])
@@ -254,7 +301,7 @@ def get_user_level(points):
 def profile():
     current_tab = request.args.get('current_tab')
     activeTab = current_tab if current_tab else 0
-    
+
     # Visited Attractions Tab
     visited_attractions = get_visited_attractions(current_user.id)
     points = sum(item['attraction'].points for item in visited_attractions)
@@ -263,8 +310,15 @@ def profile():
     # Profile Tab
     profile_form = ProfileForm()
     tags = Tag.query.all()
+    categories = Category.query.all()
+    age_groups = AgeGroup.query.all()
     profile_form.tag.choices = [(tag.id, tag.name) for tag in tags]
-    profile_form.country.choices = [(country.id, country.name) for country in Country.query.all()]
+    profile_form.category.choices = [
+        (cat.id, cat.name) for cat in categories]
+    profile_form.age_group.choices = [
+        (age.id, age.name) for age in age_groups]
+    profile_form.country.choices = [
+        (country.id, country.name) for country in Country.query.all()]
 
     if profile_form.validate_on_submit():
         activeTab = 1
@@ -272,9 +326,13 @@ def profile():
         profile_form.country.data = current_user.country_id
     else:
         # set current user settings
-        profile_form.tag.data = [tag.tag_id for tag in current_user.tag_preferences]    
+        profile_form.tag.data = [
+            tag.tag_id for tag in current_user.tag_preferences]
+        profile_form.category.data = [
+            cat.category_id for cat in current_user.category_preferences]
+        profile_form.age_group.data = [
+            age.age_group_id for age in current_user.age_group_preferences]
         profile_form.country.data = current_user.country_id
-
 
     # Fetch the user preferences from the database
     user_preferences = get_user_preferences()
@@ -297,7 +355,8 @@ def profile():
     badge_progress = get_user_badge_progress(current_user.id)
 
     # Wishlist Tab
-    user_wishlist = AttractionGroup.query.filter_by(owner=current_user.id).first()
+    user_wishlist = AttractionGroup.query.filter_by(
+        owner=current_user.id).first()
     wishlist_attractions = []
 
     if user_wishlist:
@@ -315,6 +374,8 @@ def profile():
         'profile.html',
         profile_form=profile_form,
         tags=tags,
+        categories=categories,
+        age_groups=age_groups,
         user_preferences=user_preferences,
         visited_attractions=visited_attractions,
         wishlist_attractions=wishlist_attractions,
@@ -324,7 +385,7 @@ def profile():
         tabs=tabs,
         friends_form=friends_form,
         users=users,
-        users_requesting=users_requesting, 
+        users_requesting=users_requesting,
         users_awaiting=users_awaiting,
         friends=friends,
         badge_progress=badge_progress,
@@ -332,16 +393,17 @@ def profile():
         level=level
     )
 
+
 @auth.route('/friends/send-request/<int:user_id>', methods=['POST'])
 @login_required
 def send_friend_request(user_id):
-    
+
     friendship = Friendship(
-        user_1 = current_user.id,
-        user_2 = user_id,
-        status = 'pending'
-        )
-    
+        user_1=current_user.id,
+        user_2=user_id,
+        status='pending'
+    )
+
     db.session.add(friendship)
     db.session.commit()
     flash(_('The friend request has been sent.'))
@@ -353,7 +415,8 @@ def send_friend_request(user_id):
 def accept_friend_request(user_id):
     friendship = Friendship.query.filter(
         (Friendship.user_1 == user_id) | (Friendship.user_2 == user_id),
-        (Friendship.user_1 == current_user.id) | (Friendship.user_2 == current_user.id),
+        (Friendship.user_1 == current_user.id) | (
+            Friendship.user_2 == current_user.id),
         Friendship.status == 'pending'
     ).first()
 
@@ -371,14 +434,15 @@ def accept_friend_request(user_id):
 def remove_friend_request(user_id):
     friendship = Friendship.query.filter(
         (Friendship.user_1 == user_id) | (Friendship.user_2 == user_id),
-        (Friendship.user_1 == current_user.id) | (Friendship.user_2 == current_user.id),
+        (Friendship.user_1 == current_user.id) | (
+            Friendship.user_2 == current_user.id),
         Friendship.status == 'pending'
     ).first()
 
     if friendship is None:
         abort(404)  # Friendship request not found or already accepted
-    
-    db.session.delete(friendship) 
+
+    db.session.delete(friendship)
     db.session.commit()
     flash(_('The friend request has been ended.'))
     return redirect(url_for('auth.profile', current_tab=3))
@@ -389,17 +453,19 @@ def remove_friend_request(user_id):
 def remove_friend(user_id):
     friendship = Friendship.query.filter(
         (Friendship.user_1 == user_id) | (Friendship.user_2 == user_id),
-        (Friendship.user_1 == current_user.id) | (Friendship.user_2 == current_user.id),
+        (Friendship.user_1 == current_user.id) | (
+            Friendship.user_2 == current_user.id),
         Friendship.status == 'accepted'
     ).first()
 
     if friendship is None:
         abort(404)  # Friendship request not found or already accepted
-    
-    db.session.delete(friendship) 
+
+    db.session.delete(friendship)
     db.session.commit()
     flash(_('The friend has been removed.'))
     return redirect(url_for('auth.profile', current_tab=2))
+
 
 @auth.route('/friend/profile/<int:user_id>', methods=['GET'])
 @login_required
@@ -414,10 +480,10 @@ def friend_profile(user_id):
     level = get_user_level(points)
 
     return render_template(
-         'friendProfile.html', 
-         friend=friend, 
-         visited_attractions=visited_attractions, 
-         number_of_visited_attractions=len(visited_attractions),
-         points=points,
-         level=level
-         )
+        'friendProfile.html',
+        friend=friend,
+        visited_attractions=visited_attractions,
+        number_of_visited_attractions=len(visited_attractions),
+        points=points,
+        level=level
+    )
